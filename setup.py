b74a0543331e9352a1a941e2cc3993b582e0bd2d
orel98 <35419161+orel98@users.noreply.github.com>
2022-03-15 15:47:19 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import find_packages, setup
 
+with open("./README.md") as f:
+    long_description = f.read()
+
 setup(
     name="tess_py_api",
     version="0.0.1",
+    author="Orel Kalaf",
+    author_email="orelron98@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/orel98/tess_py_api/tree/main",
+    license="MIT",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
